74c1106aede6b9d9248a65db66913701e5ea683e
Richárd Szemerédi <richard.szemeredi@icloud.com>
2021-04-24 12:17:19 +0200

    Initial commit of all files to the repository

---
+++ allasok.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
+    <title>Főoldal</title>
+</head>
+<body>
+<ul>
+    <li><a class="active" href="#home">Főoldal</a></li>
+    <li><a href="#jobs">Állások</a></li>
+    <li><a href="#inc">Cégek</a></li>
+    <li style="float:right"><a href="login.html">Login</a></li>
+</ul>
+<?php
+
+$tns = "
+(DESCRIPTION =
+    (ADDRESS_LIST =
+      (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))
+    )
+    (CONNECT_DATA =
+      (SID = xe)
+    )
+  )";
+ 
+  
+$conn = oci_connect('system', 'oracle', $tns,'UTF8');
+
+echo '<h2>Dummy lekerdezes: </h2>';
+echo '<table border="0">';
+
+
+//// -- lekerdezzuk a tabla tartalmat
+$stid = oci_parse($conn, 'SELECT * FROM DUAL');
+
+oci_execute($stid);
+
+//// -- eloszor csak az oszlopneveket kerem le
+$nfields = oci_num_fields($stid);
+echo '<tr>';
+for ($i = 1; $i<=$nfields; $i++){
+    $field = oci_field_name($stid, $i);
+    echo '<td>' . $field . '</td>';
+}
+echo '</tr>';
+
+//// -- ujra vegrehajtom a lekerdezest, es kiiratom a sorokat
+oci_execute($stid);
+
+while ( $row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) {
+    echo '<tr>';
+    foreach ($row as $item) {
+        echo '<td>' . $item . '</td>';
+    }
+    echo '</tr>';
+}
+echo '</table>';
+
+oci_close($conn);
+
+
+?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
+    <title>Főoldal</title>
+</head>
+<body>
+<ul>
+    <li><a class="active" href="index.php">Főoldal</a></li>
+    <li><a href="allasok.php">Állások</a></li>
+    <li><a href="#inc">Cégek</a></li>
+    <li style="float:right"><a href="login.html">Bejelentkezés</a></li>
+</ul>
+<h1>Üdvözüljük álláskereső oldalunkon!</h1>
+<h3>Válogasson kedve szerint a meghirdetett állások listájából!<br>
+Nálunk csak ellenörzött vállalkozások hirdetnek!
+</h3>
+<h2 id="a">Ammenyiben szeretne állásra jelentkezni, vagy
+új állást meghirdetni kérjük jelentkezzen be!</h2>
+</body>
+</html>
